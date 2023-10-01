672096b900d38dded75310daf23dab1012942aeb
parthya007 <146193646+parthya007@users.noreply.github.com>
2023-10-01 20:03:33 +0530

    Add files via upload

---
+++ 1main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hey!! Parth here\n");
+    return 0;
+}
---
+++ 4size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+// TO KNOW THE SIZE OF PARTICULAR VARIABLES:
+int main()
+{
+    printf("%lu", sizeof(int)); 
+    return 0;
+}
+
+// TO PERFORM ARITHMETIC OPERATIONS:
+// int main()
+// {
+//     printf("%d", 838*754);
+// }
+
+// TO STORE DATA IN DESIRED VARIABLE:
+// int main()
+// {
+//     int a;
+//     scanf("%d", &a);
+// }
